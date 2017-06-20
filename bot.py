2ec11a3b0aedbe14ce2569f8736b9bc507e3ea9d
EdgarFabiano <eddy.led@gmail.com>
2017-06-20 16:29:41 -0300

    Tratamento de algumas excessões

--- bot.py
+++ bot.py
@@ -1,18 +1,36 @@
+import errno
 from socket import *
 serverName = 'DESKTOP-OOVUV0L' #Nome do meu computador na rede
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
+try:
+    clientSocket.connect((serverName,serverPort))
+except error, e:
+    if e[0] == errno.ETIMEDOUT:
+        print 'Erro ao conectar com o gerente - TIMEOUT\n'
+        exit(0)
+        
+try:
+    clientSocket.send('cli'+gethostname())
+    print 'Conectado ao gerente'
+except error, e:
+    if e[0] == 10057:
+        print 'Gerente desligado'
+        exit(0)
 
 clientPort = 12001
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', clientPort))
+try:
+    serverSocket.bind(('', clientPort))
+    serverSocket.listen(1)
+except error, e:
+    if e[0] == 10048:
+        print 'A porta de comunicacao do bot (12001) esta em uso'
+        print 'Mate o processo que a utiliza para poder prosseguir'
+        #TODO: fazer esse procedimento de encontrar e matar o processo que utiliza a porta 12001 automaticamente
 
-serverSocket.listen(1)
-clientSocket.send('cli'+gethostname())
 
 while 1:
-    connectionSocket = None
     try:
         connectionSocket, addr = serverSocket.accept()
         rcv = connectionSocket.recv(1024)
