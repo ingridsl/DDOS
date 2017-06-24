936a11a811d72dceb865fa032816e49541a846a7
ingridsl <inus2agome@gmail.com>
2017-06-24 12:59:47 -0300

    começo de montando header do DNS

--- server.py
+++ server.py
@@ -12,6 +12,19 @@ ip = '127.0.0.1'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
 
+def buildresponse(data): #DNS Header
+
+	#Transaction ID
+	TransactionID = data[0:2]
+	TID = ''
+	for byte in TransactionID
+		TID += hex(byte)[2:]
+	#print(TID)
+	
+	#Flags
+	Flags = getflags(data[2:4])
 while 1:
 	data, addr = sock.recvfrom(512)
-	print(data)
+	#print(data)
+	r = buildresponse(data)
+	sock.sendto(r, addr)
